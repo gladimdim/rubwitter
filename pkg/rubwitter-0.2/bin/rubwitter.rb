ef561f3b78ed5e2664d2b9afa1d5227e84c33d91
Dmitry Gladkiy <gladimdim@gmail.com>
2010-11-30 00:47:11 +0200

    Removed pkg folder from git

--- pkg/rubwitter-0.2/bin/rubwitter.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require "twitter_oauth"
-require "yaml"
-require "open3"
-RUBWITTER_LIBRARY_PATH = File.dirname(__FILE__)  + "../lib/".to_s
-$:.unshift(RUBWITTER_LIBRARY_PATH)
-require "rubwitter"
-include Rubwitter
-Rubwitter.start_app()
